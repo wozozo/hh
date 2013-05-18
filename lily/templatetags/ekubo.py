658fa1f00e25cd59bb098f8e2d842981f3261e6a
Yoichi Fujimoto <wozozo@gmail.com>
2013-05-19 01:07:20 +0900

    ekubo template tag

--- lily/templatetags/ekubo.py
+++ lily/templatetags/ekubo.py
@@ -1,20 +1,53 @@
 # coding: utf-8
 
-# from urllib import urlencode
-# from urlparse import parse_qsl
+from urllib import urlencode
 
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 
-@register.filter
-def resize(value, params):
+class EkuboNode(template.Node):
 
-    if isinstance(value, str):
+    def __init__(self, path, params):
+        self.path = template.Variable(path)
+        self.params = params
+        self.escape = False
+
+    def render(self, context):
+
+        actual_path = self.path.resolve(context)
+        path = _get_path(actual_path)
+        url = 'http://{}/{}'.format(settings.EKUBO_DOMAIN, path)
+
+        return url + self.buildquery()
+
+    def buildquery(self):
+
+        if not len(self.params):
+            return ''
+
+        query = {}
+        for param in self.params.split(','):
+            key, val = param.split('=')
+            query[key] = val
+
+        return '?' + urlencode(query)
+
+
+@register.tag()
+def ekubo(parser, token):
+    tag_name, path, params = token.split_contents()
+
+    return EkuboNode(path, params[1:-1])
+
+
+def _get_path(value):
+    if isinstance(value, (str, unicode)):
         path = value
     else:
-        # ImageField の場合
+        # if ImageField
         path = value.name
 
-    return '{}?{}'.format(path, params)
+    return path
